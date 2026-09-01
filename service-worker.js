7f6585d63fc7683542287be2b26129e2022eeecf
topdiveair-sketch <topdiveair@gmail.com>
2026-09-01 20:03:30 +0200

    Aktualisiere Gäste-App-Cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'zab-v16-1-wachau-meister-urkunde';
+const CACHE_NAME = 'zab-v72-fakten-korrektur';
 const CORE = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg','./hero-windis.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(()=>null)); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
